6e261dcdee7f5b7bab8ffea7398de91525ea404c
Ben <ben.kahan@live.co.uk>
2019-09-01 17:18:26 -0400

    v1.1: Added centralised configuration file

---
+++ config.py
@@ -0,0 +1,27 @@
+config = {
+
+    ### Bot -----------------------
+    "bot_username" : "1882-Bot",
+    "bot_name" : "1882Bot",
+
+    ### Subreddit -----------------
+    "sub_name" : "coys",
+
+    ### Flair ---------------------
+
+    ## Global setting to moderate flair
+    "moderate_flair" : True,
+
+    # Auto reply for submissions which don't have a flair
+    "flair_submission_reply" : "\n\nIn /r/coys, we're having a trial period where we require all posts to be flaired appropriately. [You can find out more details and discuss this here](https://reddit.com/r/coys/comments/cw3n72/).",
+
+    # Append message to the end of autoreply to users who have successfully flaired their post
+    "flair_success_reply" : "COYS!",
+
+    # Append message to end of autoedit to users who did not flair their post in time
+    "flair_failure_edit" : "For more information, [see this post](https://reddit.com/r/coys/comments/cw3n72/).\n\nCOYS",
+
+    # Flair details when flairing to remove post for failure to flair
+    "flair_failure_flair_id" : "4a538d20-327a-11e8-b606-0ec2b090ed4e",
+    "flair_failure_flair_text" : "Removed by 1882-Bot"
+}
