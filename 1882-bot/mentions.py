d97ff1313893df62f113f6824b377165cc2728c8
Ben <ben.kahan@live.co.uk>
2019-09-16 07:02:22 -0400

    Took mentions method message loop and moved it into main script

--- 1882-bot/mentions.py
+++ 1882-bot/mentions.py
@@ -10,53 +10,48 @@ import datetime
 # TESTING
 import logging
 
-def messages_mentions(r, message, logfile):
-    log_it(logfile, "Checking mentions...")
-    try:
-        history = datahandler.get("mentions")
-        mods = r.subreddit(config["sub_name"]).moderator()
-        for message in r.inbox.mentions(limit=30):
-            # Get important variables
-            comment = r.comment(id=message.id)
-            msgtext = comment.body.split("\n")[0].split(" ")[1:]
-            submission = r.submission(id=comment.link_id[3:])
-            request = msgtext[0].lower()
+def messages_mentions(r, message, logfile, mods, history):
+    # Get important variables
+    comment = r.comment(id=message.id)
+    msgtext = comment.body.split("\n")[0].split(" ")[1:]
+    submission = r.submission(id=comment.link_id[3:])
+    request = msgtext[0].lower()
 
-            if comment.subreddit.display_name.lower() == config["sub_name"].lower() and comment.id not in history :
-                if request == "resetthecounter" :
-                    if comment.author in mods:
-                        log_it(logfile, "\tMod request: ResetTheCounter https://reddit.com/r/" + config["sub_name"] + "/comments/" + submission.id)
-                        reply = comment.reply(config["mention_resetthecounter_reply_mod"])
-                        reply.mod.distinguish()
-                        submission.flair.select(config["mention_resetthecounter_flair_id"], config["mention_resetthecounter_flair_text"])
+    # Execute
+    if comment.subreddit.display_name.lower() == config["sub_name"].lower() and comment.id not in history :
+        if request == "resetthecounter" :
+            if comment.author in mods:
+                log_it(logfile, "\tMod request: ResetTheCounter https://reddit.com/r/" + config["sub_name"] + "/comments/" + submission.id)
+                reply = comment.reply(config["mention_resetthecounter_reply_mod"])
+                reply.mod.distinguish()
+                submission.flair.select(config["mention_resetthecounter_flair_id"], config["mention_resetthecounter_flair_text"])
 
-                        # Do widget
-                        if config["mention_resetthecounter_widgetname"] is not "" :
-                            # Get widget
-                            widgets = r.subreddit(config["sub_name"]).widgets
-                            for widget in widgets.sidebar :
-                                if widget.shortName == config["mention_resetthecounter_widgetname"] :
-                                    log_it(logfile, "Test: found widget")
-                                    # Update and execute
-                                    newtext = generate_widget(submission, widget.text)
-                                    widget.mod.update(text=newtext[0])
-                                    log_it(logfile, "\t\tUpdated sidebar widget")
+                # Do widget
+                if config["mention_resetthecounter_widgetname"] is not "" :
+                    # Get widget
+                    widgets = r.subreddit(config["sub_name"]).widgets
+                    for widget in widgets.sidebar :
+                        if widget.shortName == config["mention_resetthecounter_widgetname"] :
+                            log_it(logfile, "Test: found widget")
+                            # Update and execute
+                            newtext = generate_widget(submission, widget.text)
+                            widget.mod.update(text=newtext[0])
+                            log_it(logfile, "\t\tUpdated sidebar widget")
 
-                                    # Send message for legacy sidebar
-                                    msgtext = "Sidebar Widget update text:  \n\n---\n\n" + newtext[0]
-                                    r.subreddit(config["sub_name"]).message("Sidebar Widget", newtext[1])
-                                    break
+                            # Send message for legacy sidebar
+                            msgtext = "Sidebar Widget update text:  \n\n---\n\n" + newtext[0]
+                            r.subreddit(config["sub_name"]).message("Sidebar Widget", newtext[1])
+                            break
 
-                    else :
-                        log_it(logfile, "\tUser request: ResetTheCounter https://reddit.com/r/" + config["sub_name"] + "/comments/" + submission.id)
-                        reply = comment.reply(config["mention_resetthecounter_reply_user"])
-                        submission.report("Possible Counter Reset situation", "[This post has been reported](https://reddit.com/r/" + config["sub_name"] + "/comments/" + submission.id +") to u/1882-Bot as a possible infringement.\n\n[Click here to send a confirmation message](https://www.reddit.com/message/compose/?to=" + config["bot_username"] + "&message=Naughty%20fuckin%20ticket%20post!&subject=ResetTheCounter:%20" + submission.id + ") or ignore to decline.")
-                        reply.mod.distinguish()
+            else :
+                log_it(logfile, "\tUser request: ResetTheCounter https://reddit.com/r/" + config["sub_name"] + "/comments/" + submission.id)
+                reply = comment.reply(config["mention_resetthecounter_reply_user"])
+                submission.report("Possible Counter Reset situation", "[This post has been reported](https://reddit.com/r/" + config["sub_name"] + "/comments/" + submission.id +") to u/1882-Bot as a possible infringement.\n\n[Click here to send a confirmation message](https://www.reddit.com/message/compose/?to=" + config["bot_username"] + "&message=Naughty%20fuckin%20ticket%20post!&subject=ResetTheCounter:%20" + submission.id + ") or ignore to decline.")
+                reply.mod.distinguish()
 
-                    datahandler.addTo("mentions", comment.id)
-                    log_it(logfile, "\t\tUpdated 'Mentions' list data: comment ID " + str(comment.id))
-    except Exception as e:
-        log_it(logfile, str(e))
+            datahandler.addTo("mentions", comment.id)
+            log_it(logfile, "\t\tUpdated 'Mentions' list data: comment ID " + str(comment.id))
+
 
 
 def message_resetthecounter(r, message, logfile) :
